13d8219d41fd7ce70259535c1249a90d5395b49e
umidkhan <umidxondeveloper@mail.ru>
2022-12-22 16:34:42 +0500

    No1 solved

---
+++ script.js
@@ -0,0 +1,16 @@
+/* 
+
+Sana va vaqt: 16:30 22-12-2022
+Muallif: Umidxon Po'latxonov
+Masala: Ayirmani toping
+
+*/
+
+function Minus(num1, num2) {
+    return num1 - num2
+}
+
+Minus(110 - 21)
+Minus(21 - 19)
+Minus(987 - 564)
+console.log(Minus());
